5c1eefd9101ffeaafbb1ba23b0bedc5eb4149b08
soulclaws <121465112+soulclaws@users.noreply.github.com>
2025-10-27 20:18:10 +0100

    circle player

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 import pygame
 from constants import *
+from player import *
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-    color = (0, 0, 0) 
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        screen.fill(color)
+
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
-        dt = clock.tick(60) / 1000
-    
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
