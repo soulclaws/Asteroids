d7e1309bfcb4567844db2f4c17b640655d6618a6
soulclaws <121465112+soulclaws@users.noreply.github.com>
2025-10-27 18:49:35 +0100

    clock added

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
-
 import pygame
 from constants import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()
+    dt = 0
     color = (0, 0, 0) 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill(color)
         pygame.display.flip()
-        
+
+        dt = clock.tick(60) / 1000
     
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
