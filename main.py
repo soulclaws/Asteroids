254c69d65ad17456da24f086f63ab1f983d05efb
soulclaws <121465112+soulclaws@users.noreply.github.com>
2025-10-30 17:09:52 +0100

    container for sprites

--- main.py
+++ main.py
@@ -7,23 +7,26 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-
     
-
-
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
+        updatable.update(dt)
+        screen.fill("black") 
         
-        screen.fill("black")
-        player.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+        
 
 
 if __name__ == "__main__":
